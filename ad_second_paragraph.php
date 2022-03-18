6840892a30143901be5cead950afde7cc7938407
devrudevelopers <100504114+devrudevelopers@users.noreply.github.com>
2022-03-18 20:07:13 +0530

    n

--- ad_second_paragraph.php
+++
@@ -1,4 +0,0 @@
-<p>
-<div style="color:#aaa;font-size:11px;">Advertising</div>
-<div style="background-color:#eee;height:90px;width:100%;"></div>
-</p>
--- footer_url.php
+++
@@ -1,6 +0,0 @@
-<a href="#">Home</a> | 
-<a href="#">Blog</a> | 
-<a href="#">Twitter</a> | 
-<a href="#">Facebook</a> | 
-<a href="#">Support</a> | 
-<a href="#">Contact Us</a>
